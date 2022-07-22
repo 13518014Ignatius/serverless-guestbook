90c5b71c490099eb5a195f4694471931b85773ae
13518014Ignatius <54546012+13518014Ignatius@users.noreply.github.com>
2022-07-22 17:59:50 +0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://963dbcba.au-syd.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
